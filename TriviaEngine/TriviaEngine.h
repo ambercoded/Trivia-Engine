37af47833a7645180b0d2a47e1614061c00514a5
Adrian <mailforadrian@me.com>
2021-03-14 13:05:13 +0100

    Initial Commit

---
+++ TriviaEngine/TriviaEngine.h
@@ -0,0 +1,18 @@
+//
+//  TriviaEngine.h
+//  TriviaEngine
+//
+//  Created by Adrian on 14.03.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TriviaEngine.
+FOUNDATION_EXPORT double TriviaEngineVersionNumber;
+
+//! Project version string for TriviaEngine.
+FOUNDATION_EXPORT const unsigned char TriviaEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TriviaEngine/PublicHeader.h>
+
+
