f1531c114715ac2d66e67378d1898fe09b8bca43
Adrian <mailforadrian@me.com>
2021-03-16 10:37:14 +0100

    more generic (no specific types in Flow) allows to use different question and answer types

--- TriviaEngine/Flow.swift
+++ TriviaEngine/Flow.swift
@@ -1,45 +1,54 @@
-//
-//  Flow.swift
-//  TriviaEngine
-//
 //  Created by Adrian on 14.03.21.
-//
 /* the flow of the application. it's a class because it's more of a behavior than data. */
 
 import Foundation
 
+// Flow doesnt care what the actual Router is implemented like (DIP)
 protocol Router {
-    typealias AnswerCallback = (String) -> Void
-    func routeTo(question: String, answerCallback: @escaping AnswerCallback)
+    // An associated type gives a placeholder name to a type that’s used as part of the protocol.
+    associatedtype Question: Hashable
+    associatedtype Answer
+
+    func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func routeTo(result: [Question: Answer])
 }
 
-class Flow {
-    private let router: Router
-    private let questions: [String]
+// only allow routers that have the same question and answer type
+class Flow<Question: Hashable, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
+    private let router: R
+    private let questions: [Question]
+    private var result: [Question: Answer] = [:]
 
-    init(questions: [String], router: Router) {
+    init(questions: [Question], router: R) {
         self.questions = questions
         self.router = router
     }
 
     func start() {
         if let firstQuestion = questions.first {
-            router.routeTo(question: firstQuestion, answerCallback: routeNext(from: firstQuestion))
+            router.routeTo(question: firstQuestion, answerCallback: nextCallback(from: firstQuestion))
+        } else { // no questions
+            router.routeTo(result: result)
         }
     }
 
-    private func routeNext(from question: String) -> Router.AnswerCallback {
-        return { [weak self] _ in
-            guard let strongSelf = self else { return }
-            if let currentQuestionIndex = strongSelf.questions.firstIndex(of: question) {
-                let nextQuestionIndex = currentQuestionIndex+1
-                if nextQuestionIndex < strongSelf.questions.count {
-                    let nextQuestion = strongSelf.questions[nextQuestionIndex]
-                    strongSelf.router.routeTo(
-                        question: nextQuestion,
-                        answerCallback: strongSelf.routeNext(from: nextQuestion))
-                }
+    private func nextCallback(from question: Question) -> (Answer) -> Void {
+        return { [weak self] in self?.routeNext(question, $0) }
+    }
+
+    private func routeNext(_ question: Question, _ answer: Answer) {
+        if let currentQuestionIndex = questions.firstIndex(of: question) {
+            result[question] = answer // set the answer for the question before routing
+
+            let nextQuestionIndex = currentQuestionIndex + 1
+            let nextQuestionExists = nextQuestionIndex < questions.count
+            if nextQuestionExists {
+                let nextQuestion = questions[nextQuestionIndex]
+                router.routeTo(question: nextQuestion, answerCallback: nextCallback(from: nextQuestion))
+            } else {
+                router.routeTo(result: result)
             }
         }
+
     }
 }
--- TriviaEngineTests/FlowTest.swift
+++ TriviaEngineTests/FlowTest.swift
@@ -1,9 +1,4 @@
-//
-//  FlowTest.swift
-//  TriviaEngineTests
-//
 //  Created by Adrian on 14.03.21.
-//
 /* test for the flow of the application. the flow is the sut (system under test). */
 
 import Foundation
@@ -18,21 +13,38 @@ class FlowTest: XCTestCase {
         XCTAssertTrue(router.routedQuestions.isEmpty)
     }
 
-    func test_start_withOneQuestion_routesToCorrectQuestion() {
-        let sut = makeSUT(questions: ["Q1"])
+    func test_start_withNoQuestions_routesToResult() {
+        makeSUT(questions: []).start()
+        XCTAssertEqual(router.routedResult, [:])
+    }
+
+    func test_start_withOneQuestion_doesNotRouteToResult() {
+        makeSUT(questions: ["Q1"]).start()
+        XCTAssertNil(router.routedResult)
+    }
+
+    func test_startAndAnswerFirstQuestion_withTwoQuestions_doesNotRouteToResult() {
+        let sut = makeSUT(questions: ["Q1", "Q2"])
         sut.start()
-        XCTAssertEqual(router.routedQuestions, ["Q1"])
+        router.answerCallback("A1")
+        XCTAssertNil(router.routedResult)
+    }
+
+    func test_startAndAnswerTwoQuestions_withTwoQuestions_routesToResult() {
+        let sut = makeSUT(questions: ["Q1", "Q2"])
+        sut.start()
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+        XCTAssertEqual(router.routedResult, ["Q1": "A1", "Q2": "A2"])
     }
 
     func test_start_withOneQuestion_routesToCorrectQuestion_2() {
-        let sut = makeSUT(questions: ["Q2"])
-        sut.start()
+        makeSUT(questions: ["Q2"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q2"])
     }
 
     func test_start_withTwoQuestions_routesToFirstQuestion() {
-        let sut = makeSUT(questions: ["Q1", "Q2"])
-        sut.start()
+        makeSUT(questions: ["Q1", "Q2"]).start()
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
 
@@ -52,28 +64,33 @@ class FlowTest: XCTestCase {
     }
 
     func test_startAndAnswerFirstQuestion_withOneQuestion_doesNotRouteToAnotherQuestion() {
-        let sut = makeSUT(questions: ["Q1"])
-        sut.start()
+        makeSUT(questions: ["Q1"]).start()
         router.answerCallback("A1")
         XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
 
     // MARK: Helpers
 
-    func makeSUT(questions: [String]) -> Flow {
+    func makeSUT(questions: [String]) -> Flow<String, String, RouterSpy> {
         return Flow(questions: questions, router: router)
     }
 
     // a mocked router for testing
     class RouterSpy: Router {
         var routedQuestions: [String] = []
-        var answerCallback: Router.AnswerCallback = { _ in }
+        var routedResult: [String: String]? = nil
+        var answerCallback: (String) -> Void = { _ in }
 
         // when a question is shown, we provide an answerCallback to send back the answer later
-        func routeTo(question: String, answerCallback: @escaping Router.AnswerCallback) {
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
             routedQuestions.append(question)
             self.answerCallback = answerCallback
         }
+
+        func routeTo(result: [String : String]) {
+            routedResult = result
+        }
+
     }
 }
 
