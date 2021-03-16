f4f7df7ca5bc655b12d6d0ef83d77c38fc5a7d71
Adrian <mailforadrian@me.com>
2021-03-16 14:00:08 +0100

    real scoring functionality and Game Module created

---
+++ TriviaEngineTests/Game.swift
@@ -0,0 +1,32 @@
+//  Created by Adrian on 16.03.21.
+// game just holds game state and calls functions for eg starting a game
+
+import Foundation
+
+public class Game<Question, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
+    let flow: Flow<Question, Answer, R>
+
+    init(flow: Flow<Question, Answer, R>) {
+        self.flow = flow
+    }
+}
+
+public func startGame<Question, Answer: Equatable, R: Router>(
+    questions: [Question],
+    router: R,
+    correctAnswers: [Question: Answer]
+) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
+
+    let flow = Flow(questions: questions, router: router, scoring: { answers in scoring(answers, correctAnswers: correctAnswers) })
+    flow.start()
+    return Game(flow: flow)
+}
+
+private func scoring<Question, Answer: Equatable>(
+    _ answers: [Question: Answer],
+    correctAnswers: [Question: Answer]
+) -> Int {
+    return answers.reduce(0) { (score, tuple) in
+        return score + (correctAnswers[tuple.key] == tuple.value ? 1 : 0)
+    }
+}
--- TriviaEngineTests/GameTest.swift
+++ TriviaEngineTests/GameTest.swift
@@ -7,16 +7,38 @@
 
 import Foundation
 import XCTest
-import TriviaEngine // necc?
+import TriviaEngine
 
 class GameTest: XCTestCase {
-    func test_startGame_answerOneOutOfTwoCorrect_scoresOne() {
-        let router = RouterSpy()
-        startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+    let router = RouterSpy()
+    var game: Game<String, String, RouterSpy>!
 
+    override func setUp() {
+        super.setUp()
+
+        game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+    }
+
+    func test_startGame_answerZeroOutOfTwoCorrect_scoresZero() {
+        router.answerCallback("wrong")
+        router.answerCallback("wrong")
+
+        XCTAssertEqual(router.routedResult!.score, 0)
+    }
+
+    func test_startGame_answerOneOutOfTwoCorrect_scoresOne() {
         router.answerCallback("A1")
         router.answerCallback("wrong")
 
         XCTAssertEqual(router.routedResult!.score, 1)
     }
+
+    func test_startGame_answerTwoOutOfTwoCorrect_scoresTwo() {
+        router.answerCallback("A1")
+        router.answerCallback("A2")
+
+        XCTAssertEqual(router.routedResult!.score, 2)
+    }
+
+
 }
